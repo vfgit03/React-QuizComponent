c325ecfecaf420f2386990f0ba185457d3fc589b
Dan <43063118+vfgit03@users.noreply.github.com>
2018-09-07 11:47:03 +0100

    Part 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,11 +17,19 @@ class Quiz extends Component {
     }));
   }
 
+  handleResetClick() {
+      this.setState({
+        quiz_position: 1
+      });
+  }
+
   render() {
     const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
     return (
       <div>
-        {isQuizEnd && <QuizEnd />}
+        {isQuizEnd && <QuizEnd
+          resetClickHandler={this.handleResetClick.bind(this)}
+         />}
         {!isQuizEnd && <QuizQuestion
           quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
           showNextQuestionHandler={this.showNextQuestion.bind(this)}
